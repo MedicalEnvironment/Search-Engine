3c3c4539dce25ac7f0de756a3d4e447e39a37fcd
Akbar Abayev <akbar.abayev@gmail.com>
2023-12-13 19:06:30 +0200

    Delete Skillbox_Search_Engine directory

--- Skillbox_Search_Engine/Skillbox_Search_Engine.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-#include "include/ConverterJSON.h"
-#include "include/InvertedIndex.h"
-#include "include/SearchServer.h"
--- Skillbox_Search_Engine/Tests/test_search.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "gtest/gtest.h"
-#include "../include/ConverterJSON.h"
-#include "../include/SearchServer.h"
-#include "../include/ConverterJSON.h"
